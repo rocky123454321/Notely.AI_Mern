bbc5784292da07b0f0c26cb96cccb05a064f0f23
rocky123454321 <rockyjayducusin3@gmail.com>
2026-02-09 19:19:55 +0800

    final

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
               className="btn btn-primary gap-2"
             >
               <PlusIcon size={18} />
-              Create Note
+              Create Notes
             </Link>
 
           {user ? (
